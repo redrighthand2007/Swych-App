bfc2ceddf70da269dd87039a224413f848d9e353
redrighthand2007 <redrighthand2007@users.noreply.github.com>
2026-09-01 13:19:56 +0530

    set default theme

--- app/src/main/java/com/kush/swych/core/util/SettingsManager.kt
+++ app/src/main/java/com/kush/swych/core/util/SettingsManager.kt
@@ -13,7 +13,7 @@ object SettingsManager {
 
     private lateinit var prefs: SharedPreferences
 
-    private val _themeMode = MutableStateFlow(2) // Default to Dark
+    private val _themeMode = MutableStateFlow(1) // Default to Light
     val themeMode: StateFlow<Int> = _themeMode.asStateFlow()
 
     private val _hapticEnabled = MutableStateFlow(true)
@@ -21,7 +21,7 @@ object SettingsManager {
 
     fun init(context: Context) {
         prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-        _themeMode.value = prefs.getInt(KEY_THEME, 2)
+        _themeMode.value = prefs.getInt(KEY_THEME, 1)
         _hapticEnabled.value = prefs.getBoolean(KEY_HAPTIC, true)
     }
 
